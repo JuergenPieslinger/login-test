d034ed9c1631862fa106464c3d8ce28b852dad3c
JuergenPieslinger <159938944+JuergenPieslinger@users.noreply.github.com>
2025-01-20 06:00:18 +0100

    First Shot

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { routes } from './app.routes';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LoginComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(routes),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.username === 'user' && this.password === 'password') {
+      this.router.navigate(['/']); // Weiterleitung nach erfolgreichem Login
+    } else {
+      this.errorMessage = 'Ungültige Anmeldedaten. Bitte versuchen Sie es erneut.';
+    }
+  }
 }
