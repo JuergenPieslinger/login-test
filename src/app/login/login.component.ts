cf64654419356d37a82360568452ab59ee47c80c
JuergenPieslinger <159938944+JuergenPieslinger@users.noreply.github.com>
2025-01-20 06:09:38 +0100

    first Shot

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  onLogin() {
+    // Simpler Loginprozess
+    if (this.username === 'admin' && this.password === 'password') {
+      alert('Login erfolgreich!');
+      this.router.navigate(['/']);
+    } else {
+      alert('Login fehlgeschlagen. Bitte prüfen Sie Ihre Eingaben.');
+    }
+  }
 }
