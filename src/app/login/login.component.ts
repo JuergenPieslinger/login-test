f0550d6f692b32fc5b9a211ab3a8429b6193e5ca
JuergenPieslinger <159938944+JuergenPieslinger@users.noreply.github.com>
2025-01-20 06:11:49 +0100

    fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
