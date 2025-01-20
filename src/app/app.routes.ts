08d7d1fa9eec1bbf07c7d6992ade45343bc5ca61
JuergenPieslinger <159938944+JuergenPieslinger@users.noreply.github.com>
2025-01-20 06:30:32 +0100

    First Shot

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'mainpage', component: MainpageComponent },
+];
